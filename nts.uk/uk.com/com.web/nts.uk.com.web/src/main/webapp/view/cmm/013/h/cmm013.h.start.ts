48f56dd4b6200e89dc38d514f37bb8d887ccce45
Hoang Duy Long <hoangduylong2100@gmail.com>
2022-04-06 11:09:09 +0700

    delete old f and h screen files

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/013/h/cmm013.h.start.ts
+++
@@ -1,8 +0,0 @@
-module nts.uk.com.view.cmm013.h {
-    __viewContext.ready(function() {
-        var screenModel = new nts.uk.com.view.cmm013.h.viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
